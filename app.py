0b0c648a61c2883a4d33b00f5a966466cf848b38
Fedorovva <dehan.drakei@gmail.com>
2025-01-11 16:23:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        # Получаем данные из запроса
+        json_str = request.get_data().decode('UTF-8')
+        # Тут нужно обработать данные и выполнить нужные действия с сообщениями
+        print(json_str)  # Для отладки, выводим полученные данные
+        return 'OK'
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))  # Получаем порт из переменной окружения
-    app.run(host='0.0.0.0', port=port)  # Привязываем приложение к порту
+    # Получаем порт из окружения или используем 5000 по умолчанию
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
