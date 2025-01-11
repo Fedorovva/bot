3d9736b0cfb48cf21c5d850c7dd7da547041c08d
Fedorovva <dehan.drakei@gmail.com>
2025-01-11 18:43:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,24 +6,8 @@ from aiogram.types import BotCommand
 import asyncio
 from datetime import datetime, timedelta
 import requests
-import os
-from flask import Flask
-from threading import Thread
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Alive"
-
-def run():
-    # Получаем порт из переменной окружения
-    port = int(os.environ.get("PORT", 10000))  # Render по умолчанию использует порт 10000
-    app.run(host='0.0.0.0', port=port)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
 
 
 
