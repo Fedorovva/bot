20c3f454b38c02d3657507f8ea19a24a04b022cd
Fedorovva <dehan.drakei@gmail.com>
2025-01-11 17:25:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,22 @@ from datetime import datetime, timedelta
 import requests
 import os
 
+from flask import Flask, render_template
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def index ():
+    return "Alive"
+
+def run ():
+  app.run(host='0.0.0.0',port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 # Токен бота
 TOKEN = "7311925613:AAEozZhlP1th7_X3LRJS_7lo3jsjy4ALHfE"
 
