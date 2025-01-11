9fbdeb752001413788ebab731c51e9f77b8fc128
Fedorovva <dehan.drakei@gmail.com>
2025-01-11 16:09:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return "Hello World!"
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.getenv('PORT', 5000))  # Используем переменную окружения PORT
+    app.run(host='0.0.0.0', port=port)   # Привязываем к порту
